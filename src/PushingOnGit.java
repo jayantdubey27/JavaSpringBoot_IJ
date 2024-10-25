091926bd7524ea5be56e5d28633c8998d75e1678
jayantdubey27 <jayantdubey27@gmail.com>
2024-10-25 13:36:28 +0530

    adding 1

---
+++ src/PushingOnGit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PushingOnGit {
+
+    public static void main(String[] args) {
+        //Printing a table
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i =1;i<=10;i++){
+            System.out.println(a*i);
+
+        }
+    }
+}
